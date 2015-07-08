c19eed69a8db131b0a21d31fdc92ec64b57eacbf
Arpit Jalan <arpit@techapj.com>
2015-07-08 11:56:34 +0530

    watch resellers category

---
+++ README.md
@@ -0,0 +1,9 @@
+discourse-watch-category-mcneel
+======================
+
+A simple plugin that watches a category for all the users in a particular group. Customized for http://discourse.mcneel.com.
+
+License
+=======
+
+MIT
--- plugin.rb
+++ plugin.rb
@@ -1,18 +1,28 @@
 # name: Watch Category
 # about: Watches a category for all the users in a particular group
-# version: 0.1
+# version: 0.2
 # authors: Arpit Jalan
-# url: https://github.com/techAPJ/discourse-watch-category
+# url: https://github.com/discourse/discourse-watch-category-mcneel
 
 module ::WatchCategory
   def self.watch_category!
     mcneel_private_category = Category.find_by_slug("mcneel-private")
     mcneel_group = Group.find_by_name("mcneel")
-    return if mcneel_private_category.nil? || mcneel_group.nil?
 
-    mcneel_group.users.each do |user|
+    unless mcneel_private_category.nil? || mcneel_group.nil?
+      mcneel_group.users.each do |user|
+        watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
+        CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], mcneel_private_category.id) unless watched_categories.include?(mcneel_private_category.id)
+      end
+    end
+
+    reseller_category = Category.find_by_slug("resellers")
+    reseller_group = Group.find_by_name("resellers")
+    return if reseller_category.nil? || reseller_group.nil?
+
+    reseller_group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], mcneel_private_category.id) unless watched_categories.include?(mcneel_private_category.id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], reseller_category.id) unless watched_categories.include?(reseller_category.id)
     end
   end
 end
